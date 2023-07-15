068ed4a475ef4d2b72791c8ba6735c0d41d0102b
Kira Khutornaya <kira.khutornaya@mail.ru>
2023-07-15 22:48:01 +0300

    Project changed

---
+++ src/components/Stars.tsx
@@ -0,0 +1,24 @@
+import { nanoid } from 'nanoid';
+import Star from './Star';
+
+interface StarsProps {
+  count: number,
+}
+
+const ratingCount: number[] = [1, 2, 3, 4, 5];
+
+function Stars({ count = 0}: StarsProps) {
+  return ratingCount.includes(count) && (
+    <ul className="card-body-stars u-clearfix">
+      <li className="card-body-star">
+      {
+        [...new Array(count)].map(() => (
+          <Star key={nanoid()} />
+        ))
+      }
+      </li>
+    </ul>
+  );
+}
+
+export default Stars;
